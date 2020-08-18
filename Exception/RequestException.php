cda394045a4acbb8a18d257bf4b3b0d6dabe34fe
Artur Doruch <arturdoruch@interia.pl>
2020-08-18 22:17:54 +0200

    RequestException: Modify normalized error message. Make constructor $message argument optional. Add @todo and @deprecated annotations

--- Exception/RequestException.php
+++ Exception/RequestException.php
@@ -7,7 +7,7 @@ use ArturDoruch\Http\Message\Response;
 use ArturDoruch\Http\Request;
 
 /**
- * HTTP Request exception
+ * HTTP request exception.
  *
  * @author Artur Doruch <arturdoruch@interia.pl>
  */
@@ -23,15 +23,48 @@ class RequestException extends \RuntimeException
      */
     private $response;
 
-    public function __construct($message, Response $response, \Exception $previous = null, Request $request = null)
+    /**
+     * @todo Not create ConnectException with this method.
+     * @todo Make $request parameter required.
+     *
+     * Creates exception with a normalized message.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param \Exception $previous
+     *
+     * @return self
+     */
+    final public static function create(Response $response, \Exception $previous = null, Request $request = null)
     {
-        $this->response = $response;
-        $this->request = $request;
-        parent::__construct($message, $response->getStatusCode(), $previous);
+        if ($response->getStatusCode() >= 500) {
+            $className = ServerException::class;
+        } elseif ($response->getStatusCode() >= 400) {
+            $className = ClientException::class;
+        } elseif (self::isConnectionError($response)) {
+            $className = ConnectException::class;
+        } else {
+            $className = static::class;
+        }
+
+        return new $className('', $response, $previous, $request);
     }
 
     /**
-     * Get the request that caused the exception
+     * @param string $message The error message. If empty, then will be created normalized.
+     * @param Response $response
+     * @param \Exception|null $previous
+     * @param Request|null $request The request caused this error.
+     */
+    public function __construct($message = '', Response $response, \Exception $previous = null, Request $request = null)
+    {
+        $this->response = $response;
+        $this->request = $request;
+        parent::__construct($message ?: self::createMessage($request, $response), $response->getStatusCode(), $previous);
+    }
+
+    /**
+     * Gets the request caused this error.
      *
      * @return Request
      */
@@ -41,17 +74,7 @@ class RequestException extends \RuntimeException
     }
 
     /**
-     * Get the associated response
-     *
-     * @return Response
-     */
-    public function getResponse()
-    {
-        return $this->response;
-    }
-
-    /**
-     * Check if request was set
+     * @deprecated
      *
      * @return bool
      */
@@ -61,84 +84,73 @@ class RequestException extends \RuntimeException
     }
 
     /**
+     * Gets request response.
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+
+    private static function createMessage(Request $request = null, Response $response)
+    {
+        $message = static::getErrorType($response) . ': ';
+        $errorMessage = trim($response->getErrorMsg());
+
+        if (0 === $statusCode = $response->getStatusCode()) {
+            $message .= $response->getErrorNumber() . ' ' . $errorMessage;
+        } else {
+            $message .= $statusCode . ' ';
+            $message .= $errorMessage ?: $response->getReasonPhrase();
+        }
+
+        $message = rtrim($message, '.,!') . ', while request ';
+
+        if ($request) {
+            $message .= 'GET ';
+        }
+
+        return $message . $response->getRequestUrl() . '.';
+    }
+
+    /**
+     * @deprecated Use method getErrorType() instead.
      * @param Response $response
+     *
      * @return string
      */
     public static function getErrorLabel(Response $response)
     {
-        $level = floor($response->getStatusCode() / 100);
-
-        if ($level == '4') {
-            return 'Client error response';
-        } elseif ($level == '5') {
-            return 'Server error response';
-        } elseif (static::isConnectionError($response)) {
-            return 'Connection error';
-        }
-
-        return 'Unsuccessful response';
-    }
-
-    /**
-     * Factory method to create a new exception with a normalized error message
-     *
-     * @param Request $request
-     * @param Response $response Response received
-     * @param \Exception $previous Previous exception
-     *
-     * @return self
-     */
-    public static function create(Response $response, \Exception $previous = null, Request $request = null)
-    {
-        if (static::isConnectionError($response)) {
-            return static::createConnect($response, $previous, $request);
-        }
-
-        $level = floor($response->getStatusCode() / 100);
-
-        if ($level == '4') {
-            $className = __NAMESPACE__ . '\\ClientException';
-        } elseif ($level == '5') {
-            $className = __NAMESPACE__ . '\\ServerException';
-        } else {
-            $className = __CLASS__;
-        }
-
-        $message = static::getErrorLabel($response)
-            . ' [url] ' . $response->getRequestUrl()
-            . ' [status code] ' . $response->getStatusCode();
-
-        $message .= $response->getStatusCode() === 0
-            ? ' [error number] ' . $response->getErrorNumber()
-            : ' [reason phrase] ' . $response->getReasonPhrase();
-
-        $errorMsg = trim($response->getErrorMsg());
-        if (!empty($errorMsg)) {
-            $message .= ' [error message] ' . $errorMsg;
-        }
-
-        return new $className($message, $response, $previous, $request);
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @param \Exception $previous
-     *
-     * @return ConnectException
-     */
-    private static function createConnect(Response $response, \Exception $previous = null, Request $request = null)
-    {
-        $message = sprintf(
-            'Connection error [url] %s [error number] %d [error message] %s',
-            $response->getRequestUrl(), $response->getErrorNumber(), $response->getErrorMsg()
-        );
-
-        return new ConnectException($message, $response, $previous, $request);
+        return static::getErrorType($response);
     }
 
     /**
      * @param Response $response
+     *
+     * @return string
+     */
+    public static function getErrorType(Response $response)
+    {
+        if ($response->getStatusCode() >= 500) {
+            return 'Server request error';
+        }
+
+        if ($response->getStatusCode() >= 400) {
+            return 'Client request error';
+        }
+
+        if (self::isConnectionError($response)) {
+            return 'Connection request error';
+        }
+
+        return 'Request error';
+    }
+
+    /**
+     * @param Response $response
+     *
      * @return bool
      */
     private static function isConnectionError(Response $response)
